d71d6c89ec5b3080a4c6a2386497196dd2f1552b
Glen Kutsovsky <gkstr1@hotmail.com>
2014-08-04 06:24:31 +0000

    add data validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
 class Review < ActiveRecord::Base
     belongs_to :user
+    validates :rating, :comment, presence: true
+    validates :rating, numericality: {
+        only_integer: true, 
+        greater_than_or_equal_to: 1, 
+        less_than_or_equal_to: 5, 
+        message: "can only be a number from 1 to 5"
+        }
 end
